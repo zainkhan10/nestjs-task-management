eb1b4a449efa7a8eaa8789517d78414b9414bd95
Zain Khan <zain.khaan147@gmail.com>
2021-07-19 17:51:58 +0500

    Added security in user password by using hash

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,6 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -10,9 +11,11 @@ import { User } from './user.entity';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
     try {
       await user.save();
     } catch (error) {
@@ -22,5 +25,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
