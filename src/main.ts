dc6d60a02068b94e7639d47b1b0ed774036aad52
Zain Khan <zain.khaan147@gmail.com>
2021-07-24 18:43:05 +0500

    Setting up production env

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if(process.env.NODE_ENV === 'development'){
     app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin })
   }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
